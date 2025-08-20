f97768dc33dd7df447407e49329acf894c4f75e7
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-08-20 23:47:35 +0530

    up

--- stayza-frontend/src/App.jsx
+++ stayza-frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import Hotelcard from './hotelcard'
-import user from './Hasal'
-import Navigation from "./components/Navigation";
-import { Button } from "./components/ui/button";
-import { hotels } from "./data"; 
+import Hero from "./components/Hero";
+import Navigation from "./components/ui/navigation";
+import HotelListings from ".";
+import Hotelcard from './hotelcard';
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Button />
-      {hotels.map((hotel) => (
-        <Hotelcard key={hotel.id} hotel={hotel} />
-      ))}
+      <Navigation /> // react fragment to group multiple elements
+      <main>
+        <div className="relative min-h-[85vh]">
+          <Hero />
+        </div>
+        <HotelListings />
+      </main>
     </>
   );
 }
+
+export default App;
---
+++ stayza-frontend/src/hotelcard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Basic Hotelcard component
+function Hotelcard() {
+  return (
+    <div className="hotelcard">
+      {/* Hotelcard content goes here */}
+      <h2>Hotel Name</h2>
+      <p>Hotel description...</p>
+    </div>
+  );
+}
+
+export default Hotelcard;
