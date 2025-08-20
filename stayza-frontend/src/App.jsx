505bf31b719068c49452e73160fdecb9508d16ca
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-08-20 22:52:31 +0530

    nav

--- stayza-frontend/src/App.jsx
+++ stayza-frontend/src/App.jsx
@@ -3,3 +3,15 @@ import user from './Hasal'
 import Navigation from "./components/Navigation";
 import { Button } from "./components/ui/button";
 import { hotels } from "./data"; 
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Button />
+      {hotels.map((hotel) => (
+        <Hotelcard key={hotel.id} hotel={hotel} />
+      ))}
+    </>
+  );
+}
