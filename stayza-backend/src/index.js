4ce1e7b57193f5717354d00e2b77058256ab7b83
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-08-20 22:38:32 +0530

    update

---
+++ stayza-backend/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json()); //convert http palyoads to js objects
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    }); 
