662a556638b06eed64f5b7b942b9c07bd48a5398
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-09-07 10:00:55 +0530

    update

--- stayza-frontend/src/main.jsx
+++ stayza-frontend/src/main.jsx
@@ -1,7 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter,Routes,Route } from "react-router";
 
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
