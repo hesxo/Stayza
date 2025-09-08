30809cb8cce285318875f5ba503b20294456ead0
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-09-08 23:27:10 +0530

    up

--- stayza-frontend/src/components/layouts/root-layout-page.jsx
+++ stayza-frontend/src/components/layouts/root-layout-page.jsx
@@ -1,11 +1,13 @@
 import Navigation from "../Navigation";
 import { Outlet } from "react-router";
+import { Toaster } from "@/components/ui/sonner";
 
 function RootLayout() {
   return (
     <>
       <Navigation />
       <Outlet />
+      <Toaster />
     </>
   );
 }
