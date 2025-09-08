e0d9d8a1e6ca9c9e8861eedd5d7b7ab622ff6b03
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-09-08 23:04:24 +0530

    update

---
+++ stayza-frontend/src/lib/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch)
--- stayza-frontend/src/main.jsx
+++ stayza-frontend/src/main.jsx
@@ -1,34 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import HomePage from "./Pages/home.page.jsx";
 import SignInPage from "./Pages/sign-in-page.jsx";
 import SignUpPage from "./Pages/sign-up-page.jsx";
 import NotFoundPage from "./Pages/not-found.page.jsx";
-import HotelPage from "./Pages/hotel.pages.jsx";
-import HotelDetailPage from "./Pages/hotel.details.page.jsx"
-import { BrowserRouter,Routes,Route } from "react-router";
 import RootLayout from "./components/layouts/root-layout-page.jsx";
+import HotelsPage from "./Pages/hotel.pages.jsx";
+import HotelDetailsPage from "./Pages/hotel.details.page.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./lib/store";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-<StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/hotels"  element={<HotelPage />} />
-          <Route path="/hotels/:_id" element={<HotelDetailPage />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
-</StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/hotels"  element={<HotelsPage />} />
+            <Route path="/hotels/:_id" element={<HotelDetailsPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
-
