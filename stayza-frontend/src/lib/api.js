9f17c45d12b96f95861591bee26df2520d6b9bf4
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-10-13 09:41:05 +0530

    fix frontend

--- stayza-frontend/src/lib/api.js
+++ stayza-frontend/src/lib/api.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/",
+    baseUrl: process.env.BACKEND_URL || "http://localhost:8000/api/",
     prepareHeaders: async (headers) => {
       return new Promise((resolve) => {
         async function checkToken() {
