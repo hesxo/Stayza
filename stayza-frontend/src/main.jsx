ae9ff4928e3cdf77b2c1209f472bf1d6817b3ebe
Hasal Dharmagunawradana <hasaldharmagunawardana@gmail.com>
2025-11-11 20:13:38 +0530

    Update main.jsx

--- stayza-frontend/src/main.jsx
+++ stayza-frontend/src/main.jsx
@@ -2,21 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import RootLayout from "./layouts/root-layout.page.jsx";
-import HomePage from "./Pages/home.page.jsx";
-import HotelDetailsPage from "./Pages/hotel-details.page.jsx";
-import HotelsPage from "./Pages/hotels.page.jsx";
-import PaymentPage from "./Pages/payment.page.jsx";
-import CompletePage from "./Pages/complete.page.jsx";
-import NotFoundPage from "./Pages/not-found.page.jsx";
-import SignInPage from "./Pages/sign-in.page.jsx";
-import SignUpPage from "./Pages/sign-up.page.jsx";
-import MyAccountPage from "./Pages/my-account.page.jsx";
+import HomePage from "./pages/home.page.jsx";
+import HotelDetailsPage from "./pages/hotel-details.page.jsx";
+import HotelsPage from "./pages/hotels.page.jsx";
+import PaymentPage from "./pages/payment.page.jsx";
+import CompletePage from "./pages/complete.page.jsx";
+import NotFoundPage from "./pages/not-found.page.jsx";
+import SignInPage from "./pages/sign-in.page.jsx";
+import SignUpPage from "./pages/sign-up.page.jsx";
+import MyAccountPage from "./pages/my-account.page.jsx";
 
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { store } from "./lib/store";
 import AdminProtectLayout from "./layouts/admin-protect.layout.jsx";
-import CreateHotelPage from "./Pages/admin/create-hotel.page.jsx";
+import CreateHotelPage from "./pages/admin/create-hotel.page.jsx";
 
 import { ClerkProvider } from "@clerk/clerk-react";
 
